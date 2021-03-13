15fed35d97661ff1cb4061e80018b7c114a17ee9
AlexanderTanJunAn <e0406248@u.nus.edu>
2021-03-13 16:08:53 +0800

    landing page with gifs

---
+++ lib/pages/firstpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  @override
+  _firstpageState createState() => _firstpageState();
+}
+
+class _firstpageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
